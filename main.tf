9a750cd8080563fe62766dbf78051b2e226ab228
pmandrekas <panman97@hotmail.com>
2023-04-19 22:28:38 +0300

    another comment

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ resource "azurerm_virtual_network" "main" {
   address_space       = ["10.0.0.0/16"]
 }
 
+#Create subnet
 resource "azurerm_subnet" "main" {
   name                 = "${var.prefix}-appserver-subnet"
   resource_group_name  = azurerm_resource_group.main.name
